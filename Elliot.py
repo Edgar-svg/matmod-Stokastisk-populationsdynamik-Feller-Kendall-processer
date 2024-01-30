26f5f2f0bcbf54b79a6dda2a8aa2156847efba29
Elliot Brunstorp <95485607+Brunstorp@users.noreply.github.com>
2024-01-30 11:55:40 +0100

    Update Elliot.py

--- Elliot.py
+++ Elliot.py
@@ -1,1 +1,21 @@
-print("test")
+
+import numpy as np
+import matplotlib as plt
+
+time = 100 #Number of time intervals
+
+'''
+1. Place yourself immediately after and event
+has occurred and compute the new populations.
+
+2. Compute R = 􏰀α Wα(X). Generate an exponentially
+distributed time T for the next event, with parameter R.
+3. Generate a uniformly distributed 
+number s in [0,R] and pick which event
+has occurred as follows: if 􏰀β−1 Wα(X) ≤ s < 􏰀β Wα(X) we
+say that event β has occurred.
+4. Update Time, events and populations,
+and repeat the process.
+'''
+
+
